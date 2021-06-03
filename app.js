88871cd1897c563d004afafc2e98a94280b3a79a
Aryan Sharma <72251324+Sharmaryan@users.noreply.github.com>
2021-06-03 17:09:22 +0530

    updated app.js

---
+++ app.js
@@ -0,0 +1,23 @@
+let button = document.querySelector("#btn-translate");
+let textInput = document.querySelector("#text-input");
+let outputDiv = document.querySelector("#output");
+let serverURL = "https://api.funtranslations.com/translate/dolan.json";
+
+function getTranslationURL(text) {
+  return serverURL + "?" + "text=" + text;
+}
+
+function errorHandler() {
+  alert("something went wrong with the server");
+}
+function clickHandler() {
+  let inputText = textInput.value;
+  fetch(getTranslationURL(inputText))
+    .then((reponse) => response.json())
+    .then((json) => {
+      let translatedText = json.contents.translated;
+      outputDiv.innerText = translatedText;
+    })
+    .catch(errorHandler);
+}
+button.addEventListener("click", clickHandler);
